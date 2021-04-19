0c385967a3b54278fc9fbd8ee58e94c86861db68
Paweł Kopel <pawel.kopel2@gmail.com>
2021-04-19 18:41:46 +0200

    add displaying replies to station

--- src/main/scala/com/example/MonitoringStation.scala
+++ src/main/scala/com/example/MonitoringStation.scala
@@ -10,12 +10,18 @@ object MonitoringStation {
 }
 
 class MonitoringStation(val name: String, context: ActorContext[Response]) extends AbstractBehavior[Response](context) {
+  val queries: Map[QueryId, Int] = Map()
+
   override def onMessage(msg: Response): Behavior[Response] =
     msg match {
-      case Response(queryId, status, percentage) =>
-        val errorNumber = status.values.count(status => status == NAVIGATION_ERROR && status == PROPULSION_ERROR)
+      case Response(queryId, statusMap, _) =>
+        val time = System.currentTimeMillis() - queries.get(queryId)
+        val errors = statusMap.filter { case (_, e) => e == NAVIGATION_ERROR && e == PROPULSION_ERROR }
+        val errorNumber = errors.size
+        println(s"station: $name")
+        println(s"response time: $time")
+        println(s"number of errors: $errorNumber")
+        errors.foreach { case (id, stat) => println(s"$id: $stat") }
         this
     }
-
-
 }
